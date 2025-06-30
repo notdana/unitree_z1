d6cde335e2600101f07cb68da9ea9a3233ce3f92
notdana <notdanaforsure@gmail.com>
2025-06-30 10:22:57 +0530

    update

--- Joint_Control.py
+++ Joint_Control.py
@@ -10,13 +10,14 @@ arm =  unitree_arm_interface.ArmInterface(hasGripper=True)
 armState = unitree_arm_interface.ArmFSMState
 arm.loopOn()
 
+#This goes to a saved position of "0.0, 1.5, -1.0, -0.54, 0.0, 0.0"
 arm.labelRun("forward")
 arm.startTrack(armState.JOINTCTRL)
 
 jnt_speed = 1.0
 for i in range(0, 1000):
     # dp = directions * speed; include 7 joints
-    arm.jointCtrlCmd([0,0,0,-1,0,0,-1], jnt_speed)
+    arm.jointCtrlCmd([0.0, 1.5, -1.0, -0.54, 0.0, 0.0], jnt_speed)
     time.sleep(arm._ctrlComp.dt)
 
 arm.backToStart()
