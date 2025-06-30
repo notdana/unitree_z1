bd69351fe9f7173b378f761569fccc5802d4773b
notdana <notdanaforsure@gmail.com>
2025-06-30 10:27:40 +0530

    update

--- Joint_Control.py
+++ Joint_Control.py
@@ -14,11 +14,16 @@ arm.loopOn()
 arm.labelRun("forward")
 arm.startTrack(armState.JOINTCTRL)
 
+#jointCtrlCmd sends joint velocity commands to the robot arm.
+#It is used when the arm is in joint velocity control mode (JOINTCTRL).
+#You must call it repeatedly (in a loop) to keep the arm moving
+
 jnt_speed = 1.0
 for i in range(0, 1000):
     # dp = directions * speed; include 7 joints
-    arm.jointCtrlCmd([0.0, 1.5, -1.0, -0.54, 0.0, 0.0], jnt_speed)
+    arm.jointCtrlCmd([-1,0,0,0,0,0,0], jnt_speed)
     time.sleep(arm._ctrlComp.dt)
+
 
 arm.backToStart()
 arm.loopOff()
