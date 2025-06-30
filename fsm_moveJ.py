8c5a8861792899e196dc11e0da7ee623543c846b
notdana <notdanaforsure@gmail.com>
2025-06-30 10:45:23 +0530

    d

--- fsm_moveJ.py
+++ fsm_moveJ.py
@@ -8,7 +8,6 @@ np.set_printoptions(precision=3, suppress=True)
 
 arm =  unitree_arm_interface.ArmInterface(hasGripper=True)
 armState = unitree_arm_interface.ArmFSMState
-
 arm.loopOn()
 
 #This goes to a saved position of "0.0, 1.5, -1.0, -0.54, 0.0, 0.0"
@@ -19,10 +18,11 @@ arm.labelRun("forward")
 # arm.MoveJ(joint_positions, gripper_position, speed)
 gripper_pos = 0.0
 jnt_speed = 2.0
+
 arm.MoveJ([0.0, 1.5, -1.0, -0.54, 0.0, 0.0], gripper_pos, jnt_speed)
+time.sleep(10)
+arm.MoveJ([1.0, 1.5, -1.0, -0.54, 0.0, 0.0], gripper_pos, jnt_speed)
 
-
-#time.sleep(10)
 
 arm.backToStart()
 arm.loopOff()
